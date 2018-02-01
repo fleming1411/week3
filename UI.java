f95cb01410a2e5fc048c41b152e175c424f1ac2a
callum.wreglesworth <w16008830@northumbria.ac.uk>
2018-02-01 16:21:36 +0000

    TEst commit 3

--- UI.java
+++ UI.java
@@ -1,4 +1,5 @@
-
+import java.util.*;
+import java.io.*;
 /**
  * A simple UI for week 3
  * 
@@ -7,5 +8,23 @@
  */
 public class UI
 {
+    private Scanner BufferedReader;
+    private int numStudents;
     
+    public void NewUI()
+    {
+       
+        {
+            BufferedReader cons=new BufferedReader(new InputStreamReader(System.in)); 
+            System.out.println("Enter the size of the student group:");
+            
+        }
+        
+
+
+    }
 }
+
+
+
+
